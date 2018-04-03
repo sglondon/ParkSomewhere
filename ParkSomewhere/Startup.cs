86b51eeb17aa643735b0abe1621e8a544668d02e
sglondon <susangs2@gmail.com>
2018-04-03 13:59:05 -0400

    first commit

---
+++ ParkSomewhere/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ParkSomewhere.Startup))]
+namespace ParkSomewhere
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
